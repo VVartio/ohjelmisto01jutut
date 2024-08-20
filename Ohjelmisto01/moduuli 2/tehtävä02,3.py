61033bb28f135e7fe8c2fbe37da2121d0a8a0b92
Ville Vartio <ville.vartio@metropolia.fi>
2024-08-20 14:59:29 +0300

    lisätty: tehtävä 2.3

---
+++ Ohjelmisto01/moduuli 2/tehtävä02,3.py
@@ -0,0 +1,6 @@
+korkeus = int(input("korkeus:"))
+kanta = int(input("kanta:"))
+piiri = korkeus*2+kanta*2
+pintaala = korkeus*kanta
+print("suorakulmion piiri :",piiri)
+print("suorakulmion pinta-ala :",pintaala)
