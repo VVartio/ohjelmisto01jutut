387f1081f8176326dab563263547bd84a33d819d
Ville Vartio <ville.vartio@metropolia.fi>
2024-08-20 14:42:04 +0300

    lisätty: kansiot eri moduuleille

--- Ohjelmisto01/moduuli 2/tehtävä02,2.py
+++ Ohjelmisto01/moduuli 2/tehtävä02,2.py
@@ -1,5 +1,5 @@
 import math
 r = input("ympyrän säde? :")
-pinta = float(r)
-pintaala = int(math.pi*pinta**2)
+pinta = int(r)
+pintaala = math.pi*pinta**2
 print("ympyrän pinta-ala on noin:", pintaala)
