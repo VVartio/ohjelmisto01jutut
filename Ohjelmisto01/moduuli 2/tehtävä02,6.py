dbc5274bbddd51efdedd5dd7e3e990f50ba95a66
Ville Vartio <ville.vartio@metropolia.fi>
2024-08-21 13:57:08 +0300

    lisätty: tehtävä 2.6

---
+++ Ohjelmisto01/moduuli 2/tehtävä02,6.py
@@ -0,0 +1,4 @@
+import random
+onetoten = [random.randint(1,9) for i in range(3)]
+dice = [random.randint(1,6) for i in range(4)]
+print(f"kolminumeroinen koodi 1-9: {onetoten} nelinumeroinen koodi 1-6: {dice}")
