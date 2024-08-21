74d91b67dad9c9ca85efdf17b68efbf76b858b71
Ville Vartio <ville.vartio@metropolia.fi>
2024-08-21 14:22:15 +0300

    patch notes: pytcharm halusi lisätä välilyöntejä koodiin

--- Ohjelmisto01/moduuli 2/tehtävä02,5.py
+++ Ohjelmisto01/moduuli 2/tehtävä02,5.py
@@ -1,10 +1,10 @@
 lemaara = float(input("kuinka monta leiviskää?\n"))
 namaara = float(input("kuinka monta naulaa?\n"))
 lumaara = float(input("kuinka monta luotia?\n"))
-luoti = lumaara*13.3
-naula = namaara*13.3*32
-leiviska = lemaara*13.3*32*20
-yhteispaino = luoti+naula+leiviska
-kgpaino = yhteispaino//1000
-gpaino = yhteispaino%1000
-print(f'massa nykymittojen mukaan: \n {kgpaino} kilogrammaa ja {gpaino} grammaa')
+luoti = lumaara * 13.3
+naula = namaara * 13.3 * 32
+leiviska = lemaara * 13.3 * 32 * 20
+yhteispaino = luoti + naula + leiviska
+kgpaino = yhteispaino // 1000
+gpaino = yhteispaino % 1000
+print(f'massa nykymittojen mukaan: \n{kgpaino} kilogrammaa ja {gpaino} grammaa')
