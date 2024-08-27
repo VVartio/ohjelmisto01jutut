299ea5de63890e9cc8054b184f706f20a3e4a71f
Ville Vartio <ville.vartio@metropolia.fi>
2024-08-27 10:35:55 +0300

    patch notes: tehtävä 3.2 lisätty

---
+++ Ohjelmisto01/moduuli 3/tehtävä 3.2.py
@@ -0,0 +1,11 @@
+hytti = input("Mikä on hyttiluokkasi?\n:")
+if hytti == "LUX" or hytti == "lux":
+    print("LUX on parvekkeellinen hytti yläkannella.")
+elif hytti == "A" or hytti == "a":
+    print("A on ikkunallinen hytti autokannen yläpuolella.")
+elif hytti == "B" or hytti == "b":
+    print("B on ikkunaton hytti autokannen yläpuolella.")
+elif hytti == "C" or hytti == "c":
+    print("C on ikkunaton hytti autokannen alapuolella.")
+else :
+    print("Virheellinen hyttiluokka.")
