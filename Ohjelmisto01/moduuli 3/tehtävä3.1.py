8ffd3571bb6991545a38d3116d7ff3eff2033fe4
Ville Vartio <ville.vartio@metropolia.fi>
2024-08-27 10:17:49 +0300

    patch notes: lisätty tehtävä 3.1

---
+++ Ohjelmisto01/moduuli 3/tehtävä3.1.py
@@ -0,0 +1,8 @@
+pituus = int(input("kuinka monta senttimetriä pitkä kuha on?\n:"))
+if pituus <= 37 and pituus != 36:
+    print("heitä takaisin veteen")
+    print(f"tarvitset vielä: {37-pituus} senttimetriä")
+elif pituus == 36 :
+    print("heitä takaisin veteen, tarvitset vielä yhden senttimetrin.")
+else :
+    print("saa syödä!")
