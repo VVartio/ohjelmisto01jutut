202a1aea517b69f7310d0485d4395a3e9cd7113c
Ville Vartio <ville.vartio@metropolia.fi>
2024-09-03 10:57:39 +0300

    patch notes: päivitetty tehtävä 5.2

--- Ohjelmisto01/Moduuli 5/tehtävä 5.2.py
+++ Ohjelmisto01/Moduuli 5/tehtävä 5.2.py
@@ -4,6 +4,7 @@ while listadder != "":
     listadder = input("anna numero tai lopeta panamalla ENTER\n:")
     if listadder == "":
         break
+    listadder = int(listadder)
     list.append(listadder)
-list.sort(reverse=True)
+list.sort(reverse = True)
 print(list[0:4])
